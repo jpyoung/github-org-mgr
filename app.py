4cce51664fe745520db656fb3d9445af0615ef28
Jack Young <jackp@vayoungs.us>
2018-07-14 17:35:55 -0400

    Ported to Python 2.7

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ import json
 data = file_util.read_in_json_file("data/example_json.json")
 
 print(data)
+
+expected_data = {
+    "author": "John Young",
+    "date": "07-14-2018",
+    "repo_name": "github-org-mr"
+}
+
+print(expected_data)
--- tests/test_file_util.py
+++ tests/test_file_util.py
@@ -12,14 +12,23 @@ class MyTestCase(unittest.TestCase):
         y = "Jack"
         self.assertEqual(x, y)
 
-    def test_read_string(self):
+    def test_read_in_json_file(self):
+        data = file_util.read_in_json_file("data/4312example_json.json")
+        print(data)
+        expected_data = {
+            "author": "John Young",
+            "date": "07-14-2018",
+            "repo_name": "github-org-mr"
+        }
+        self.assertEqual(data, expected_data)
+
+    def test_read_in_json_file(self):
         data = file_util.read_in_json_file("data/example_json.json")
         expected_data = {
             "author": "John Young",
             "date": "07-14-2018",
             "repo_name": "github-org-mgr"
         }
-
 
 if __name__ == '__main__':
     unittest.main()
