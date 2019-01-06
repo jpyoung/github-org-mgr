99280c2c74bd403a642e376881ce4a8829224199
Jack Young <jackp@vayoungs.us>
2019-01-06 15:40:36 -0500

    Refactored Requestable class

--- requestable.py
+++ requestable.py
@@ -1,6 +1,9 @@
 __author__ = 'john.young'
 import requests
-
+import os
+import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Requestable(object):
 
@@ -8,8 +11,7 @@ class Requestable(object):
         self._baseAPI = baseAPI
         self._auth = auth
 
-
-    def _getURL(self, api_route):
+    def __get_url(self, api_route):
         """
         Return the full URL for API Call
         :param api_route:
@@ -17,20 +19,46 @@ class Requestable(object):
         """
         return self._baseAPI + api_route
 
-    def __getHeader(self):
+    def __get_header(self):
+        """
+        Return the request header using the auth token provided to the constructor
+
+        :return: dict - header
+        """
+
         header = {
             'accept': "application/vnd.github.v3+json",
             'content-type': "application/json",
-            'authorization': "token {}".format(str(self._auth.token)),
+            'authorization': "Basic {}".format(str(self._auth['token'])),
         }
         return header
 
     def _request(self, method, path, data=None):
+        """
+        Generic function for making HTTP requests to the API
 
-        full_url = self._getURL(path)
+        :param method: [POST, GET, PATCH]
+        :param path:
+        :param data:
+        :return:
+        """
+
+        full_url = self.__get_url(path)
         payload = data
-        response = requests.request(method, full_url, payload)
+        reponse = requests.request(method, full_url, headers=self.__get_header())
         return reponse
+
+
+    def listLabels(self, repository):
+        """
+        List the labels for a given repository
+
+        :param repository: org/repo
+        :return:
+        """
+        return self._request("GET", "/repos/{}/labels".format(str(repository)))
+
+
 
 
 	
